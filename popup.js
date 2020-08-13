30ca629cdfdb62c51db84e56d9c7eabed2019457
Graeme <gwoodhouse@gmail.com>
2020-08-13 16:00:49 -0400

    Fixed to work on all tabs. No fluff.

--- popup.js
+++ popup.js
@@ -3,8 +3,8 @@ let mergeButton = document.getElementById('mergeButton');
 console.log("Setting Clicker");
 mergeButton.onclick = function(element) {
     console.log("button clicked");
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log("tabs: " + chrome.tabs);
+    chrome.tabs.query({}, function(tabs) {
+        console.log("tabs: " + tabs);
         for(var i = 0; i < tabs.length; i++) {
             chrome.tabs.executeScript(
                 tabs[i].id,
